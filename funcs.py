365b10de5d3d894a1ceaee22bf9d6754b62906f0
LenSin3 <lenpk3@yahoo.co.uk>
2021-09-12 03:38:46 -0700

    add another condition to include beverage filter in barplot

--- funcs.py
+++ funcs.py
@@ -236,6 +236,7 @@ def cat_plots(df_melt, *args, **kwargs):
 
     """
     n = kwargs.get('n', None)
+    beverage = kwargs.get('beverage', None)
     if 'boxplot' in args:
         fig, ax = plt.subplots()
         fig.set_size_inches(15, 10)
@@ -256,6 +257,19 @@ def cat_plots(df_melt, *args, **kwargs):
         plt.title('Top {} Regions by Mean Sales per Capita'.format(n))
         # plt.savefig("images/top_{}_regions.png".format(n))
         plt.show()
+    elif (n and beverage) and 'barplot' in args:
+        sort_df_bev = df_melt.loc[df_melt['beverages'] == beverage]
+        sort_df = sort_df_bev.sort_values(by = ['Sales per Capita'], ascending = False)
+        top_n = sort_df.iloc[:n, :]
+        fig, ax = plt.subplots()
+        fig.set_size_inches(15, 10)
+        sns.set_context('poster', font_scale = 0.5, rc = {'grid.linewidth': 0.5})
+        sns.barplot(x = 'Sales per Capita', y = 'region', data = top_n)
+        sns.despine(offset = 10, trim = True)
+        plt.title('Top {} Regions by Mean Sales per Capita of {}'.format(n, beverage))
+        # plt.savefig("images/top_{}_regions.png".format(n))
+        plt.show()
+
 
 def corr_heatmap(df):
     """
